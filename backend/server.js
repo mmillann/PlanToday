41ec87058246b93e690e3a78968de4108f26667b
Angel <angelgx298@gmail.com>
2023-04-24 21:05:59 +0200

    vuelta atras

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(
-    cors({
-        origin: "http://15.236.18.180:3000",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true,
-        preflightContinue: true,
-    })
-);
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
 
 app.get("/", (req, res) => {
     res.json("Backend funcionando");
@@ -36,6 +20,6 @@ app.use("/planes", planesController);
 app.use("/comentarios", comentariosController);
 app.use("/imagenes", imagenesController);
 
-app.listen(8080, "http://15.236.18.180", () => {
+app.listen(8080, () => {
     console.log("server.js funcionando");
 });
