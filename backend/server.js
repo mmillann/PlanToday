7a4f22fdd653c176441183813a0cd2cf2e23b9ac
Angel <angelgx298@gmail.com>
2023-04-24 20:22:53 +0200

    prevuelo añadido

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,17 @@ app.use(
         origin: "http://localhost:3000",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+        preflightContinue: true,
     })
 );
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 app.get("/", (req, res) => {
     res.json("Backend funcionando");
