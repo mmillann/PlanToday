1d20158227feb09d1185408dfcd3a3679e3519cc
Angel <angelgx298@gmail.com>
2023-04-24 21:04:14 +0200

    0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "http://0.0.0.0:3000",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
@@ -36,6 +36,6 @@ app.use("/planes", planesController);
 app.use("/comentarios", comentariosController);
 app.use("/imagenes", imagenesController);
 
-app.listen(8080, () => {
+app.listen(8080, "0.0.0.0", () => {
     console.log("server.js funcionando");
 });
