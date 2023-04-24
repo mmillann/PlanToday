4b32365366f05925651b25243ca735810622d0ac
Angel <angelgx298@gmail.com>
2023-04-24 20:17:58 +0200

    cors puerto 3000 añadidos

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 
 app.get("/", (req, res) => {
     res.json("Backend funcionando");
