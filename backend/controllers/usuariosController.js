788a7dd8b15487122c461e915a439b3ec493ede8
Angel <angelgx298@gmail.com>
2023-05-11 00:36:43 +0200

    register funcionando

--- backend/controllers/usuariosController.js
+++ backend/controllers/usuariosController.js
@@ -27,16 +27,22 @@ router.get("/user/:correo", (req, res) => {
     const { correo } = req.params;
     const query = "SELECT * FROM usuarios WHERE correo = ?";
     db.query(query, [correo], (err, data) => {
-      if (err) return res.json(err);
-      return res.json(data);
+        if (err) return res.json(err);
+        return res.json(data);
     });
-  });
-  
+});
+
 // agregar un nuevo usuario
 router.post("/", (req, res) => {
     console.log("Body recibido: ", req.body);
     const { nombre_usuario, nombre_completo, correo, password } = req.body;
-    console.log("Valores de campos: ", nombre_usuario, nombre_completo, correo, password);
+    console.log(
+        "Valores de campos: ",
+        nombre_usuario,
+        nombre_completo,
+        correo,
+        password
+    );
     const query = `INSERT INTO usuarios (nombre_usuario, nombre_completo, correo, password) VALUES ("${nombre_usuario}", "${nombre_completo}", "${correo}", "${password}");`;
     db.query(query, (err, data) => {
         if (err) return res.json(err);
@@ -69,19 +75,44 @@ router.delete("/:id", (req, res) => {
 router.post("/login", (req, res) => {
     const { correo, password } = req.body;
     const query = `SELECT * FROM usuarios WHERE correo = "${correo}" AND password = "${password}";`;
-  
+
     db.query(query, (err, data) => {
-      if (err) return res.json(err);
-  
-      if (data.length > 0) {
-        // el usuario y la contraseña son válidos, devolver los datos del usuario
-        const usuario = data[0];
-        return res.json({ message: "Inicio de sesión exitoso", usuario });
-      } else {
-        // las credenciales son inválidas
-        return res.status(401).json({ message: "Credenciales inválidas" });
-      }
+        if (err) return res.json(err);
+
+        if (data.length > 0) {
+            // el usuario y la contraseña son válidos, devolver los datos del usuario
+            const usuario = data[0];
+            return res.json({ message: "Inicio de sesión exitoso", usuario });
+        } else {
+            // las credenciales son inválidas
+            return res.status(401).json({ message: "Credenciales inválidas" });
+        }
     });
-  });
+});
+
+// agregar un nuevo usuario
+router.post("/registro", (req, res) => {
+  const { correo, password, nombreUsuario, nombreCompleto } = req.body;
+  const query =
+      "INSERT INTO usuarios (correo, password, nombre_usuario, nombre_completo, admin, avatar) VALUES (?, ?, ?, ?, false, 'default')";
+  db.query(
+      query,
+      [correo, password, nombreUsuario, nombreCompleto],
+      (err, result) => {
+          if (err) {
+              console.error(err);
+              return res
+                  .status(500)
+                  .json({
+                      message: "Ocurrió un error al registrar el usuario",
+                  });
+          }
+          return res
+              .status(200)
+              .json({ message: "Usuario registrado exitosamente" });
+      }
+  );
+});
+
 
 export default router;
