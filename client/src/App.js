0b97b9b447833db3633a51c03bd1e9f24caf8f0c
Angel <angelgx298@gmail.com>
2023-05-25 20:30:08 +0200

    page de cada categoria creado y linkeado

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,15 @@ import React from "react";
 import "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Otro from "./pages/Categorias/Otro";
+import Viajes from "./pages/Categorias/Viajes";
+import Musica from "./pages/Categorias/Musica";
+import Gastronomia from "./pages/Categorias/Gastronomia";
+import Fiestas from "./pages/Categorias/Fiestas";
+import Educacion from "./pages/Categorias/Educacion";
+import Deportes from "./pages/Categorias/Deportes";
+import Cine from "./pages/Categorias/Cine";
+import Arte from "./pages/Categorias/Arte";
 
 function App() {
     return (
@@ -16,6 +25,15 @@ function App() {
             <Route path="/" element={<Home/>}/>
             <Route path="/perfil/:id" element={<Perfil/>}/>
             <Route path="/explorar" element={<Explorar/>}/>
+            <Route path="/explorar/arte" element={<Arte/>}/>
+            <Route path="/explorar/cine" element={<Cine/>}/>
+            <Route path="/explorar/deportes" element={<Deportes/>}/>
+            <Route path="/explorar/educacion" element={<Educacion/>}/>
+            <Route path="/explorar/fiestas" element={<Fiestas/>}/>
+            <Route path="/explorar/gastronomia" element={<Gastronomia/>}/>
+            <Route path="/explorar/musica" element={<Musica/>}/>
+            <Route path="/explorar/viajes" element={<Viajes/>}/>
+            <Route path="/explorar/otro" element={<Otro/>}/>
         </Routes>
         </BrowserRouter>
         
--- client/src/pages/Explorar.jsx
+++ client/src/pages/Explorar.jsx
@@ -1,72 +1,112 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";  // Agrega esta línea
+import { useParams, Link } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
-import { FaUserCircle } from 'react-icons/fa';
 import Galeria from "../components/Galeria/Galeria";
-import LoginModal from "../components/LoginModal/LoginModal";
 import Slidebar from "../components/Slidebar/Slidebar";
 import axios from "axios";
+import "./Explorar.css"; // Importa el archivo CSS personalizado
 
-function Perfil() {
-  const nombre = sessionStorage.getItem("nombre");
-  const [users, setUsers] = useState([]);
-  const { id } = useParams();
+function Explorar() {
+    const [users, setUsers] = useState([]);
+    const { id } = useParams();
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get("http://localhost:8080/usuarios");
-        setUsers(res.data);
-        console.log(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchUsers();
-  }, []);
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const res = await axios.get("http://localhost:8080/usuarios");
+                setUsers(res.data);
+                console.log(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchUsers();
+    }, []);
 
-  const getNombreCreador = (id) => {
-    const user = users.find((user) => id === user.id.toString()); // Convertir `id` a una cadena antes de comparar
-    if (user) {
-      return user.nombre_usuario; // Cambiar `user.nombre_usuario` por `user.nombre_completo` para obtener el nombre completo del creador
-    } else {
-      return "username";
-    }
-  };
-
-  const getNombreCompletoCreador = (id) => {
-    const user = users.find((user) => id === user.id.toString()); // Convertir `id` a una cadena antes de comparar
-    if (user) {
-      return user.nombre_completo; // Cambiar `user.nombre_usuario` por `user.nombre_completo` para obtener el nombre completo del creador
-    } else {
-      return "username";
-    }
-  };
-
-  console.log(id);
-  return (
-
-    <div className="d-flex-column justify-content-center">
-    <div className="container-fluid position-fixed fixed-top cab">
-      <Navbar />
-    </div>
-    <div className="mt-3">
-    <div className="slidebar mt-5">
-      <Slidebar />
-    </div>
-    <div className="d-flex justify-content-center">
-        <div className="infoPerfil d-flex flex-column mt-5">
-
-          /* Contenido aqui */
-
+    return (
+        <div className="d-flex-column justify-content-center">
+            <div className="container-fluid position-fixed fixed-top cab">
+                <Navbar />
+            </div>
+            <div className="mt-3">
+                <div className="slidebar mt-5">
+                    <Slidebar />
+                </div>
+                <div className="d-flex justify-content-center align-items-center h-100">
+                    <div className="infoPerfil d-flex flex-column mt-5 center-container">
+                        <div className="categorias-container row">
+                            <div className="col-4">
+                                <Link to="/explorar/deportes">
+                                    <button className="btn btn-primary categoria-button">
+                                        Deportes
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="col-4">
+                                <Link to="/explorar/arte">
+                                    <button className="btn btn-primary categoria-button">
+                                        Arte
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="col-4">
+                                <Link to="/explorar/musica">
+                                    <button className="btn btn-primary categoria-button">
+                                        Música
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="col-4">
+                                <Link to="/explorar/gastronomia">
+                                    <button className="btn btn-primary categoria-button">
+                                        Gastronomía
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="col-4">
+                                <Link to="/explorar/viajes">
+                                    <button className="btn btn-primary categoria-button">
+                                        Viajes
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="col-4">
+                                <Link to="/explorar/cine">
+                                    <button className="btn btn-primary categoria-button">
+                                        Cine
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="col-4">
+                                <Link to="/explorar/fiestas">
+                                    <button className="btn btn-primary categoria-button">
+                                        Fiestas
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="col-4">
+                                <Link to="/explorar/educacion">
+                                    <button className="btn btn-primary categoria-button">
+                                        Educación
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="col-4">
+                                <Link to="/explorar/otro">
+                                    <button className="btn btn-primary categoria-button">
+                                        Otro
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="galeria">
+                    <Galeria idUsuario={id} />
+                </div>
+            </div>
         </div>
-        </div>
-        <div className="galeria">
-          <Galeria idUsuario={id}/>
-        </div>
-        </div>
-  </div>
-  );
+    );
 }
 
-export default Perfil;
+export default Explorar;
